3267c27d34369d58555b7a1ce796c7fd33fe706f
Fipc123 <cdcfipc@gmail.com>
2025-05-14 09:02:47 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+document.getElementById("Soy Un Boton").addEventListener("click",CambioColor);
+
+let color;
+function CambioColor()
+{
+    if(color == true)
+    {
+        document.body.style.backgroundColor = "red";
+        color = false;
+    }
+    else
+    {
+        document.body.style.backgroundColor = "blue";
+        color = true;
+    }
+}
